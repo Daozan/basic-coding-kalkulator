df5e97daa7f427791cca0b5ffbbdf8aff7230c9f
Daozan <ayatosama112@gmail.com>
2024-02-23 11:25:28 +0700

    first time

---
+++ nur aliya fitri nur solehah/src/nur/aliya/fitri/nur/solehah/NurAliyaFitriNurSolehah.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nur.aliya.fitri.nur.solehah;
+
+/**
+ *
+ * @author SERVER
+ */
+public class NurAliyaFitriNurSolehah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b,c,d;
+        
+        //proses
+        a=2;
+        b=3;
+        c=1;
+        
+        d=a+b+c;
+        //output
+     System.out.println("Nilai d adalah"+d);
+    }
+    
+}
